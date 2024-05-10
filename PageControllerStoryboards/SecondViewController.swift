a838bab1e5e9f612c7fcb0629bf2cc92f1ab91ec
Sandy Gomez <sgomez1029@gmail.com>
2024-05-09 20:26:31 -0700

    Sharing Data Between View Controllers

--- PageControllerStoryboards/SecondViewController.swift
+++ PageControllerStoryboards/SecondViewController.swift
@@ -5,25 +5,28 @@
 //  Created by Sandra Gomez on 5/6/24.
 //
 
+
 import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet var myTextField: UITextField!
+    
+    static let notificationName = Notification.Name("myNotification")
+    
+    @objc func onNotification(notification: Notification) {
+        let data = notification.object
+        let temp = String(describing: data!)
+        myTextField.text = temp
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(onNotification(notification:)), name: ViewController.notificationName, object: nil)
     }
-    */
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        let dataSent = myTextField.text
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "myNotification"), object: dataSent)
+    }
 }
--- PageControllerStoryboards/ViewController.swift
+++ PageControllerStoryboards/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myTextField: UITextField!
+    
+    static let notificationName = Notification.Name("myNotification")
+    
+    @objc func onNotification(notification: Notification) {
+        let data = notification.object
+        let temp = String(describing: data!)
+        myTextField.text = temp
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(onNotification(notification:)), name: ViewController.notificationName, object: nil)
     }
-
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        let dataSent = myTextField.text
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "myNotification"), object: dataSent)
+    }
 }
 
